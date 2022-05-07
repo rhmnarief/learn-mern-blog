c41c08937eab19917d6aaca766b9e13a38a14970
Arief <rhmnarief7@gmail.com>
2022-05-07 17:40:46 +0700

    add GUI for login pages

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import {Button, Gap, Input, Link} from '../../components'
+import { LoginBg } from '../../assets'
 
 const Login = () => {
   return (
-      <div>
-          <p>
-          login pages
-          </p>
+         <div className='main-page'>
+        <div className='left'>
+            <img src={LoginBg} className="bg-image" alt="imageBg" />
+        </div>
+        <div className='right'>
+          <p className='title'>Login</p>
+          <Gap height={18}/>
+          <Input label="Email" placeholder="Email" />
+          <Gap height={18} />
+          <Input label="Password" placeholder="Password" />
+          <Gap height={50} />
+          <Button title="Login" />
+          <Gap height={100} />
+          <Link title="Belum punya akun, silahkan daftar"/>
+        </div>
       </div>
   )
 }
