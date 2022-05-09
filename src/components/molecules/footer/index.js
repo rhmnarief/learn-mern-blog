c45bf82528f9aaa56c566892520f14b996562fb4
Arief <rhmnarief7@gmail.com>
2022-05-09 13:36:34 +0700

    add footer on main app pages

--- src/components/molecules/footer/index.js
+++ src/components/molecules/footer/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { ICDiscord, ICFacebook, ICGithub, ICInstagram, ICTwitter, LogoKabayan } from '../../../assets';
 import './footer.scss';
 
+const Icon = ({img}) =>{
+    return (
+        <div className='icon-wrapper'>
+            <img className='icon-medsos' alt='icon' src={img}></img>
+        </div>
+    )
+}
 
 const Footer = () => {
     return (
         <div>
             <div className="footer">
+                <div>
+                    <img className="logo" src={LogoKabayan} alt="Logo Kabayan Coding" />
+                </div>
                 <div className="social-wrapper">
-                    <p>Footer</p>
+                    <Icon img={ICFacebook} />
+                    <Icon img={ICTwitter} />
+                    <Icon img={ICInstagram} />
+                    <Icon img={ICDiscord} />
+                    <Icon img={ICGithub} />
                 </div>
             </div>
             <div className="copyright">            
